e81bd593bfb4202cea7f1c53e510a05408467b23
Emad Khatrush <qwe.emad@hotmail.com>
2024-09-03 11:37:20 +0300

    change preview image of uae address

--- src/containers/StartShipmentPage/ShipmentGuide.tsx
+++ src/containers/StartShipmentPage/ShipmentGuide.tsx
@@ -251,7 +251,7 @@ const fromUAE = (data: any, account: User, state: any) => {
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description" className='text-end'>
-            <img src="https://storage.googleapis.com/exios-bucket/uae-address.png" alt="" />
+            <img src="https://storage.googleapis.com/exios-bucket/lTWvolTk9vnew-uae-address-02.png" alt="" />
           </DialogContentText>
         </DialogContent>
         <DialogActions>
